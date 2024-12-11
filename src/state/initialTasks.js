74ccce9e915d18867db2da9038245176d25a5a1a
bairojusathwika <102368330+bairojusathwika@users.noreply.github.com>
2024-12-12 00:14:58 +0530

    Add files via upload

---
+++ src/state/initialTasks.js
@@ -0,0 +1,9 @@
+const initialTasks = [
+    { id: 1, title: "Task 1", description: "Description 1", status: "To Do" },
+    { id: 2, title: "Task 2", description: "Description 2", status: "In Progress" },
+    { id: 3, title: "Task 3", description: "Description 3", status: "Peer Review" },
+    { id: 4, title: "Task 4", description: "Description 4", status: "Done" },
+  ];
+  
+  export default initialTasks;
+  
